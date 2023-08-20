1e6078491e46190474f32bb736fc96a9b31ae551
sasantana <sergio.santos@dcomp.ufs.br>
2023-08-20 13:51:23 -0300

    :teste: feat: criacao  de teste autenticacao e login

---
+++ test/testeAutenticacao.test.ts
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import supertest from "supertest";
+import app from "../src/app"; 
+
+describe("Autenticação de Usuário", () => {
+  beforeAll(() => {
+    // Suprimir saídas de log durante os testes
+    console.log = jest.fn();
+  });
+
+  afterAll(() => {
+    // Restaurar a saída de log
+    console.log = console._log;
+  });
+
+  it("deve permitir que um usuário válido faça login", async () => {
+    // Simular a autenticação com credenciais válidas
+    const response = await request.post("/login").send({
+      email: "usuario@example.com",
+      password: "senha",
+    });
+
+    // Verificar o código de status e a mensagem flash
+    expect(response.status).toBe(302); // Redirecionamento após o login
+    expect(response.headers.location).toBe("/"); // Redirecionamento para a página inicial
+    expect(response.headers["set-cookie"]).toBeDefined(); // Cookies definidos
+  });
+
+  it("não deve permitir o login com credenciais inválidas", async () => {
+    // Simular a autenticação com credenciais inválidas
+    const response = await request.post("/login").send({
+      email: "usuario@example.com",
+      password: "senha_incorreta",
+    });
+
+    // Verificar o código de status e a mensagem flash
+    expect(response.status).toBe(302); // Redirecionamento após falha de login
+    expect(response.headers.location).toBe("/login"); // Redirecionamento de volta para a página de login
+    expect(response.headers["set-cookie"]).toBeUndefined(); // Cookies não definidos
+  });
+});
+
+mongoose.connect = jest.fn();
+
+// Configuração do Supertest
+const request = supertest(app);
+
+describe("Autenticação de Usuário", () => {
+  beforeAll(() => {
+    // Suprimir saídas de log durante os testes
+    console.log = jest.fn();
+  });
+
+  afterAll(() => {
+    // Restaurar a saída de log
+    console.log = console._log;
+  });
+
+  it("deve permitir que um usuário válido faça login", async () => {
+    // Simular a autenticação com credenciais válidas
+    const response = await request.post("/login").send({
+      email: "usuario@example.com",
+      password: "senha",
+    });
+
+    // Verificar o código de status e a mensagem flash
+    expect(response.status).toBe(302); // Redirecionamento após o login
+    expect(response.headers.location).toBe("/"); // Redirecionamento para a página inicial
+    expect(response.headers["set-cookie"]).toBeDefined(); // Cookies definidos
+  });
+
+  it("não deve permitir o login com credenciais inválidas", async () => {
+    // Simular a autenticação com credenciais inválidas
+    const response = await request.post("/login").send({
+      email: "usuario@example.com",
+      password: "senha_incorreta",
+    });
+
+    // Verificar o código de status e a mensagem flash
+    expect(response.status).toBe(302); // Redirecionamento após falha de login
+    expect(response.headers.location).toBe("/login"); // Redirecionamento de volta para a página de login
+    expect(response.headers["set-cookie"]).toBeUndefined(); // Cookies não definidos
+  });
+});
---
+++ test/testeLogin.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import { User, UserDocument } from "../src/models/User";
+import app from "../src/app";
+
+describe("Testes de Login", () => {
+    let user: UserDocument;
+
+    beforeAll(async () => {
+        // Cria um usuário de teste no banco de dados
+        user = await User.create({
+            email: "teste@example.com",
+            password: "testepassword",
+        });
+    });
+
+    afterAll(async () => {
+        // Limpa os dados de teste após os testes
+        await User.deleteMany({});
+    });
+
+    it("Deve fazer login com sucesso", async () => {
+        const response = await request(app)
+            .post("/login")
+            .send({
+                email: user.email,
+                password: "testepassword",
+            });
+
+        expect(response.status).toBe(302); // Verifica se o redirecionamento ocorreu
+        expect(response.header.location).toBe("/"); // Verifica se o usuário foi redirecionado para a página inicial
+    });
+
+    it("Deve tratar login com informações inválidas", async () => {
+        const response = await request(app)
+            .post("/login")
+            .send({
+                email: user.email,
+                password: "errorpassword",
+            });
+
+        expect(response.status).toBe(302); // Verifica se o redirecionamento ocorreu
+        expect(response.header.location).toBe("/login"); // Verifique se o usuário foi redirecionado de volta para a página de login
+    });
+
+    it(" Trata login com email inválido", async () => {
+        const response = await request(app)
+            .post("/login")
+            .send({
+                email: "invalid@example.com",
+                password: "testpassword",
+            });
+
+        expect(response.status).toBe(302); // Verifica se o redirecionamento ocorreu
+        expect(response.header.location).toBe("/login"); // Verifica se o usuário foi redirecionado de volta para a página de login
+    });
+});
